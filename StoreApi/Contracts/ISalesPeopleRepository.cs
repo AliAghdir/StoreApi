44cb8d3e38117460257ede756cb9025185e2bcf2
Ali Taghi Aghdir <alitaghiaghdir@gmail.com>
2022-05-27 13:10:20 +0330

    Create Repository Pattern For SalesPeople

---
+++ StoreApi/Contracts/ISalesPeopleRepository.cs
@@ -0,0 +1,14 @@
+﻿using StoreApi.Models;
+
+namespace StoreApi.Contracts
+{
+    public interface ISalesPeopleRepository
+    {
+        IEnumerable<SalesPerson> GetAll();
+        Task<SalesPerson> Add(SalesPerson sales);
+        Task<SalesPerson> Find(int id);
+        Task<SalesPerson> Update(SalesPerson sales);
+        Task<SalesPerson> Remove(int id);
+        Task<bool> IsExists(int id);
+    }
+}
---
+++ StoreApi/Repositories/SalesPeopleRepository.cs
@@ -0,0 +1,54 @@
+﻿using StoreApi.Contracts;
+using StoreApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace StoreApi.Repositories
+{
+    public class SalesPeopleRepository : ISalesPeopleRepository
+    {
+        private StoreDbContext _context;
+
+        public SalesPeopleRepository(StoreDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<SalesPerson> GetAll()
+        {
+            return _context.SalesPeople.ToList();
+        }
+
+        public async Task<SalesPerson> Add(SalesPerson sales)
+        {
+            await _context.SalesPeople.AddAsync(sales);
+            await _context.SaveChangesAsync();
+            return sales;
+        }
+
+        public async Task<SalesPerson> Find(int id)
+        {
+            return await _context.SalesPeople.SingleOrDefaultAsync(s => s.SalesPersonId == id);
+
+        }
+
+        public async Task<SalesPerson> Update(SalesPerson sales)
+        {
+            _context.Update(sales);
+            await _context.SaveChangesAsync();
+            return sales;
+        }
+
+        public async Task<SalesPerson> Remove(int id)
+        {
+            var sales = await _context.SalesPeople.SingleAsync(s => s.SalesPersonId == id);
+            _context.SalesPeople.Remove(sales);
+            await _context.SaveChangesAsync();
+            return sales;
+        }
+
+        public async Task<bool> IsExists(int id)
+        {
+            return await _context.SalesPeople.AnyAsync(s => s.SalesPersonId == id);
+        }
+    }
+}
